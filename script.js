dbe2100c88e7391ac94bdb001bd6d3f96f11bd49
okaginting <okaginting3@gmail.com>
2025-06-30 15:28:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,11 @@
 let nama = "Oka Ginting";
 let profesi = "Frontend Developer Pemula";
 let tahun = 2025;
+let alasan = "karena ingin jadi Web Developer remote";
 
 // 2. Operator
 let usia = tahun - 1988; // misal kamu lahir 1988
 
 // 3. Output ke layar
 alert("Halo, saya " + nama + ". Usia saya " + usia + " tahun.");
-
+alert(alasan):

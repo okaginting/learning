42b421d0ba724749fae2dec97cf14315d356f3ed
okaginting <okaginting3@gmail.com>
2025-07-01 10:52:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@
 //let teks = document.getElementById("sapaan");
 
 // Ubah teksnya
-teks.innerText = "Halo! Selamat datang di website Oka Ginting.";
+//teks.innerText = "Halo! Selamat datang di website Oka Ginting.";
 window.onload = function() {
   let teks = document.getElementById("sapaan");
   teks.innerText = "Halo! Selamat datang di website Oka Ginting.";

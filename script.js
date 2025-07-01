c442deb3109b15b55692bd949bddccbcc6ff40cf
okaginting <okaginting3@gmail.com>
2025-07-01 08:24:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 // Ini komentar di JavaScript
 
-// 1. Variabel
-//let nama = "Oka Ginting";
-//let profesi = "Frontend Developer Pemula";
-//let tahun = 2025;
-//let alasan = "karena ingin jadi Web Developer remote";
+ 1. Variabel
+let nama = "Oka Ginting";
+let profesi = "Frontend Developer Pemula";
+let tahun = 2025;
+let alasan = "karena ingin jadi Web Developer remote";
 
-// 2. Operator
-//let usia = tahun - 1988; // misal kamu lahir 1988
+ 2. Operator
+let usia = tahun - 1988; // misal kamu lahir 1988
 
-// 3. Output ke layar
-//alert("Halo, saya " + nama + ". Usia saya " + usia + " tahun.");
-//alert(alasan)://
+ 3. Output ke layar
+alert("Halo, saya " + nama + ". Usia saya " + usia + " tahun.");
+alert(alasan)://
 
 // Minta nama dari user
 let namaUser = prompt("Halo! Siapa nama kamu?");

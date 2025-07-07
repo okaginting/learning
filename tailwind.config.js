e1894db4c77f295656dc09c80e568e9e878a5d04
okaginting <okaginting3@gmail.com>
2025-07-07 14:49:14 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,3 +2,7 @@ module.exports = {
   darkMode: 'class', // atau 'media' jika ingin pakai preferensi sistem
   // ...
 }
+
+document.getElementById("darkModeToggle").addEventListener("click", function() {
+  document.documentElement.classList.toggle("dark");
+});
